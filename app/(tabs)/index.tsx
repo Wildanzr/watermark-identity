9aa7cd5cd86143a15bc40971ab10ac8c5be45d37
Danzrrr <graita.sukma@gmail.com>
2024-07-18 23:00:40 +0700

    cfg: nativewind

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,6 +4,7 @@ import { HelloWave } from "@/components/HelloWave";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
+import { Link } from "expo-router";
 
 export default function HomeScreen() {
   return (
@@ -50,6 +51,10 @@ export default function HomeScreen() {
           <ThemedText type="defaultSemiBold">app-example</ThemedText>.
         </ThemedText>
       </ThemedView>
+
+      <Link href="meong">
+        <ThemedText>Go to Meong</ThemedText>
+      </Link>
     </ParallaxScrollView>
   );
 }
---
+++ app/meong.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const HomeView = () => {
+  return (
+    <View className="items-center justify-center w-full h-screen p-5 bg-slate-900">
+      <Text className="text-2xl font-bold text-white">HomeView</Text>
+    </View>
+  );
+};
+
+export default HomeView;
